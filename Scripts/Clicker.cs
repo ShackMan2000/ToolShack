08bf71b6b8cc22ca5f4c91d91f729637485777ce
Julian <julianpieper@gmx.de>
2023-11-14 18:51:50 +0100

    disable clicker, needed the new input system, add more geometry extensiond

--- Scripts/Clicker.cs
+++ Scripts/Clicker.cs
@@ -3,75 +3,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
+//using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class Clicker : MonoBehaviour
 {
 
-    Camera cam;
-
-    Controls controls;
-
-    public static event Action<Vector3> ClickedNewTarget = delegate { };
-
-    PointerEventData pointerData;
-
-    [SerializeField] GraphicRaycaster raycaster;
-
-    private void Awake()
-    {
-        controls = new Controls();
-        controls.Enable();
-        cam = Camera.main;
-
-        pointerData = new PointerEventData(null);
-    }
-
-    private void OnEnable()
-    {
-        controls.Touch.MobileClick.performed += CheckForClickableHits;
-    }
-
-
-
-    private void CheckForClickableHits(InputAction.CallbackContext obj)
-    {
-        // check if input is over UI element 
-        // pointer over UI only works with releasing finger, so doing this workaround
-
-        pointerData.position = controls.Touch.MobileClick.ReadValue<Vector2>();
-
-        List<RaycastResult> raycastResults = new List<RaycastResult>();
-
-        raycaster.Raycast(pointerData, raycastResults);
-
-        if (raycastResults.Count > 0)
-            return;
-
-
-        Ray ray = cam.ScreenPointToRay(controls.Touch.MobileClick.ReadValue<Vector2>());
-
-        if (Physics.Raycast(ray, out RaycastHit hit))
-        {
-            IMoveTarget target = hit.collider.GetComponent<IMoveTarget>();
-
-            if (target != null)
-            {
-                target.Clicked();
-                if (target.MoveToCenter)
-                    ClickedNewTarget(target.Position);
-                else
-                    ClickedNewTarget(hit.point);
-            }
-
-        }
-
-    }
-
-    private void OnDisable()
-    {
-        controls.Disable();
-    }
+   //  Camera cam;
+   //
+   // // Controls controls;
+   //
+   //  public static event Action<Vector3> ClickedNewTarget = delegate { };
+   //
+   //  PointerEventData pointerData;
+   //
+   //  [SerializeField] GraphicRaycaster raycaster;
+   //
+   //  private void Awake()
+   //  {
+   //      controls = new Controls();
+   //      controls.Enable();
+   //      cam = Camera.main;
+   //
+   //      pointerData = new PointerEventData(null);
+   //  }
+   //
+   //  private void OnEnable()
+   //  {
+   //      controls.Touch.MobileClick.performed += CheckForClickableHits;
+   //  }
+   //
+   //
+   //
+   //  private void CheckForClickableHits(InputAction.CallbackContext obj)
+   //  {
+   //      // check if input is over UI element 
+   //      // pointer over UI only works with releasing finger, so doing this workaround
+   //
+   //      pointerData.position = controls.Touch.MobileClick.ReadValue<Vector2>();
+   //
+   //      List<RaycastResult> raycastResults = new List<RaycastResult>();
+   //
+   //      raycaster.Raycast(pointerData, raycastResults);
+   //
+   //      if (raycastResults.Count > 0)
+   //          return;
+   //
+   //
+   //      Ray ray = cam.ScreenPointToRay(controls.Touch.MobileClick.ReadValue<Vector2>());
+   //
+   //      if (Physics.Raycast(ray, out RaycastHit hit))
+   //      {
+   //          IMoveTarget target = hit.collider.GetComponent<IMoveTarget>();
+   //
+   //          if (target != null)
+   //          {
+   //              target.Clicked();
+   //              if (target.MoveToCenter)
+   //                  ClickedNewTarget(target.Position);
+   //              else
+   //                  ClickedNewTarget(hit.point);
+   //          }
+   //
+   //      }
+   //
+   //  }
+   //
+   //  private void OnDisable()
+   //  {
+   //      controls.Disable();
+   //  }
 
 }
--- Scripts/ExtensionsGeometry.cs
+++ Scripts/ExtensionsGeometry.cs
@@ -1,34 +1,83 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public static class ExtensionsGeometry
 {
+    
+   
+     public static Vector3 AdjustBy(this Vector3 vec, float? x = null, float? y = null, float? z = null)
+     {
+         float newX = x == null ? vec.x : vec.x + (float)x;
+         float newY = y == null ? vec.y : vec.y + (float)y;
+         float newZ = z == null ? vec.z : vec.z + (float)z;
 
-    public static Vector3 AdjustBy(this Vector3 vec, float? x = null, float? y = null, float? z = null)
-    {
-        float newX = x == null ? vec.x : vec.x + (float)x;
-        float newY = y == null ? vec.y : vec.y + (float)y;
-        float newZ = z == null ? vec.z : vec.z + (float)z;
+         return new Vector3(newX, newY, newZ);
+     }
 
-        return new Vector3(newX, newY, newZ);
-    }
+     public static Vector3 Set(this Vector3 vec, float? x = null, float? y = null, float? z = null)
+     {
+         float newX = x ?? vec.x;
+         float newY = y ?? vec.y;
+         float newZ = z ?? vec.z;
 
-    public static Vector3 Set(this Vector3 vec, float? x = null, float? y = null, float? z = null)
-    {
-        float newX = x ?? vec.x;
-        float newY = y ?? vec.y;
-        float newZ = z ?? vec.z;
-
-        return new Vector3(newX, newY, newZ);
-    }
+         return new Vector3(newX, newY, newZ);
+     }
 
 
-    public static Vector3 DirectionTo(this Vector3 origin, Vector3 target)
-    {
-        return Vector3.Normalize(target - origin);
-    }
+     public static Vector3 DirectionTo(this Vector3 origin, Vector3 target)
+     {
+         return Vector3.Normalize(target - origin);
+     }
 
-    public static Vector3 DirectionTo(this Transform origin, Transform target)
-    {
-        return origin.position.DirectionTo(target.position);
-    }
+     public static Vector3 DirectionTo(this Transform origin, Transform target)
+     {
+         return origin.position.DirectionTo(target.position);
+     }
+
+
+
+     public static Vector3 Random(this Vector3 original, float min, float max)
+     {
+
+         return new Vector3(UnityEngine.Random.Range(min, max), UnityEngine.Random.Range(min, max), UnityEngine.Random.Range(min, max));
+     }
+
+
+     public static Object GetByChance(this Dictionary<Object, int> dic)
+     {
+         List<Object> list = new List<Object>();
+
+         foreach (var item in dic)
+         {
+             for (int i = 0; i < item.Value; i++)
+             {
+                 list.Add(item.Key);
+             }
+         }
+
+         if (list.Count > 0)
+         {
+             return list[UnityEngine.Random.Range(0, list.Count)];
+         }
+         else
+         {
+             Debug.Log("Trying to pick from a dictionary that doesn't have any entries");
+             return null;
+         }
+     }
+
+
+
+
+     public static float GetHueDifference(this float hueA, float hueB)
+     {
+         float dif = Mathf.Abs(hueA - hueB);
+         if (dif > 0.5f)
+             dif = 1f - dif;
+
+         return dif * 2f;
+     }
+
+
+   
 }
